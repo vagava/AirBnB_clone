24de420621dabac08056c1560e72e92e4a3ff690
vagava <vagava.123@gmail.com>
2021-02-14 22:17:19 -0500

    add interactivceinteractive mode and non-interactive mode (line 45), and add the function create(line 13

--- console.py
+++ console.py
@@ -3,12 +3,25 @@
 
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """  command interpreter """
     prompt = "(hbnb)"
 
+    def do_create(self, args):
+        """Creates a new instance of the clase name given
+        as an argument"""
+        if args:
+            try:
+                new_instance = globals()[args]()
+                new_instance.save()
+                print(new_instance.id)
+            except:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
     def emptyline(self):
         """No realiza ninguna accion
@@ -27,5 +40,9 @@ class HBNBCommand(cmd.Cmd):
 
 """inicio del programa"""
 if __name__ == '__main__':
+    import sys
     interprete = HBNBCommand()
-    interprete.cmdloop() 
+    if len(sys.argv) > 1: # activa el modo no interactivo
+        interprete.onecmd(' '.join(sys.argv[1:]))
+    else:
+        interprete.cmdloop() # modo interactivo
