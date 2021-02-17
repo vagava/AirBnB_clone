dcddfc588ac67031ececd81cf8b682199b3e834a
jdanielue <jdurregoe@gmail.com>
2021-02-17 14:00:24 -0500

    class amenity creation basic attributes

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class amenity"""
+
+import uuid
+import datetime
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class amenity"""
+    name = ""
+
