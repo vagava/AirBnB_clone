e564278b4cf73169dc5e3a499efd92f9d86e413c
jdanielue <jdurregoe@gmail.com>
2021-02-17 14:23:40 -0500

    updated imports console Cap letter point 10

--- console.py
+++ console.py
@@ -7,11 +7,11 @@ import shlex
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models.amenity import amenity
-from models.city import city
-from models.place import place
-from models.review import review
-from models.state import state
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
