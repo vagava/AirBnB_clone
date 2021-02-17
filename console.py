06d3b521c29bffe0abe47e1663237c2f0a2fd3fa
vagava <vagava.123@gmail.com>
2021-02-17 11:58:55 -0500

    por si algo

--- console.py
+++ console.py
@@ -4,8 +4,9 @@
 
 import cmd
 import shlex
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
